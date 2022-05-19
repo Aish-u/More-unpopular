55139c5dbee5b60c639fa14c4e53f37e49bf65b3
villrv <ashley.villar@gmail.com>
2022-05-19 15:26:36 -0400

    looping over a galaxy added

--- pipelinefiles/testrun.py
+++ pipelinefiles/testrun.py
@@ -37,34 +37,43 @@ s1 = tess_cpm.Source(path_to_FFIs[0], remove_bad=True)
 
 
 # Need a piece of code to grid over TESS
-delta_row = 0
-delta_col = -4
-s1.set_aperture(rowlims=[24-delta_row, 26-delta_row], collims=[24 - delta_col, 26 - delta_col])
-_ = s1.plot_cutout(rowlims=[20, 30], collims=[20, 30], show_aperture=True)
-_ = s1.plot_pix_by_pix()  # This method plots the raw flux values
-_ = s1.plot_pix_by_pix(data_type="normalized_flux")
-s1.add_cpm_model(exclusion_size=5, n=64, predictor_method="similar_brightness")
-s1.add_poly_model(scale=2, num_terms=4)
-s1.set_regs([0.01, 0.1])  # The first term is the CPM regularization while the second term is the polynomial regularization value.
-s1.holdout_fit_predict(k=50);  # When fitting with a polynomial component, we've found it's better to increase the number of sections.
-s1.plot_pix_by_pix(data_type="poly_model_prediction", split=True);
-s1.plot_pix_by_pix(data_type="cpm_subtracted_flux");
-s1_aperture_normalized_flux = s1.get_aperture_lc(data_type="normalized_flux")
-s1_aperture_cpm_prediction = s1.get_aperture_lc(data_type="cpm_prediction")
-s1_aperture_poly_prediction = s1.get_aperture_lc(data_type="poly_model_prediction")
-plt.plot(s1.time, s1_aperture_normalized_flux, ".", c="k", ms=8, label="Normalized Flux")
-plt.plot(s1.time, s1_aperture_cpm_prediction, "-", lw=3, c="C3", alpha=0.8, label="CPM Prediction")
-plt.plot(s1.time, s1_aperture_poly_prediction, "-", lw=3, c="C0", alpha=0.8, label="Polynomial Prediction")
 
-plt.xlabel("Time - 2457000 [Days]", fontsize=30)
-plt.ylabel("Normalized Flux", fontsize=30)
-plt.tick_params(labelsize=20)
-plt.legend(fontsize=30)
-plt.show()
-s1_aperture_detrended_flux = s1.get_aperture_lc(data_type="cpm_subtracted_flux")
-plt.plot(s1.time, s1_aperture_detrended_flux, "k-")
-# plt.plot(s1.time, s1_aperture_normalized_flux-aperture_cpm_prediction, "r.", alpha=0.2)  # Gives you the same light curve as the above line
-plt.xlabel("Time - 2457000 [Days]", fontsize=30)
-plt.ylabel("CPM Flux", fontsize=30)
-plt.tick_params(labelsize=20)
-plt.show()
+for delta_row in [-4,-3,-2,-1,0,1,2,3,4]:
+    for delta_col in [-4, -3, -2, -1, 0, 1, 2, 3,4]:
+        aux_plotting = False
+        final_plotting = True
+        s1.set_aperture(rowlims=[24-delta_row, 26-delta_row], collims=[24 - delta_col, 26 - delta_col])
+        if aux_plotting:
+            _ = s1.plot_cutout(rowlims=[20, 30], collims=[20, 30], show_aperture=True)
+            _ = s1.plot_pix_by_pix()  # This method plots the raw flux values
+            _ = s1.plot_pix_by_pix(data_type="normalized_flux")
+        s1.add_cpm_model(exclusion_size=5, n=64, predictor_method="similar_brightness")
+        s1.add_poly_model(scale=2, num_terms=4)
+        s1.set_regs([0.01, 0.1])  # The first term is the CPM regularization while the second term is the polynomial regularization value.
+        s1.holdout_fit_predict(k=50);  # When fitting with a polynomial component, we've found it's better to increase the number of sections.
+        if aux_plotting:
+            s1.plot_pix_by_pix(data_type="poly_model_prediction", split=True);
+            s1.plot_pix_by_pix(data_type="cpm_subtracted_flux");
+        s1_aperture_normalized_flux = s1.get_aperture_lc(data_type="normalized_flux")
+        s1_aperture_cpm_prediction = s1.get_aperture_lc(data_type="cpm_prediction")
+        s1_aperture_poly_prediction = s1.get_aperture_lc(data_type="poly_model_prediction")
+        if aux_plotting:
+            plt.plot(s1.time, s1_aperture_normalized_flux, ".", c="k", ms=8, label="Normalized Flux")
+            plt.plot(s1.time, s1_aperture_cpm_prediction, "-", lw=3, c="C3", alpha=0.8, label="CPM Prediction")
+            plt.plot(s1.time, s1_aperture_poly_prediction, "-", lw=3, c="C0", alpha=0.8, label="Polynomial Prediction")
+
+            plt.xlabel("Time - 2457000 [Days]", fontsize=30)
+            plt.ylabel("Normalized Flux", fontsize=30)
+            plt.tick_params(labelsize=20)
+            plt.legend(fontsize=30)
+            plt.show()
+        s1_aperture_detrended_flux = s1.get_aperture_lc(data_type="cpm_subtracted_flux")
+        if final_plotting:
+            plt.plot(s1.time, s1_aperture_detrended_flux, "k-")
+            # plt.plot(s1.time, s1_aperture_normalized_flux-aperture_cpm_prediction, "r.", alpha=0.2)  # Gives you the same light curve as the above line
+            plt.xlabel("Time - 2457000 [Days]", fontsize=30)
+            plt.ylabel("CPM Flux", fontsize=30)
+            plt.tick_params(labelsize=20)
+            plt.tight_layout()
+            plt.savefig('ra'+str(ra)+'dec'+str(dec)+'delr'+str(delta_row)+'delc'+str(delta_col)+'.png')
+            plt.clf()
